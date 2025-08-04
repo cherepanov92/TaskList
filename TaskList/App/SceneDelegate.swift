32f8eb3a7a6528129284a14f08a952532fcbca48
Cherepanov Artyom <cherepanov92@gmail.com>
2025-08-04 22:34:49 +0500

    Вынес работу с CoreData в StorageManager

--- TaskList/App/SceneDelegate.swift
+++ TaskList/App/SceneDelegate.swift
@@ -7,9 +7,10 @@
 
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    private let storageManager = StorageManager.shared
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
@@ -19,9 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+        storageManager.saveContext()
     }
-
-
 }
 
---
+++ TaskList/StorageManager/StorageManager.swift
@@ -0,0 +1,36 @@
+//
+//  StorageManager.swift
+//  TaskList
+//
+//  Created by User on 04.08.2025.
+//
+
+import CoreData
+
+final class StorageManager {
+    static let shared = StorageManager()
+    
+    private init() {}
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "TaskList")
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
